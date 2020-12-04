88dc2dcc5859d4011214018a0fe09a4cb43ad73c
adamteaney@gmail.com <eproboraiders@gmail.com>
2020-12-03 20:04:36 -0600

    started box A autonomous with roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tests/RoadrunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tests/RoadrunnerTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.autonomous.tests;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -16,22 +17,27 @@ public class RoadrunnerTest extends LinearOpMode {
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        // Pose2d startPose = new Pose2d(-63, 48, Math.toRadians(0));
+        //from the roadrunner example they wanted their robot to be in a certain starting position
+        //Pose2d startPose = new Pose2d(-63, 48, Math.toRadians(0));
 
-        // drive.setPoseEstimate(startPose);
+        //drive.setPoseEstimate(startPose);
 
-        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                .splineToConstantHeading(new Vector2d(64, 12), Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(48, 12), Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(72, -12), Math.toRadians(0))
-
+        Trajectory traj1 = drive.trajectoryBuilder(new Pose2d())
+                //first trajectory moves the first wobble goal into box A
+                //every time the robot stops, a new trajectory must be made
+                .splineToConstantHeading(new Vector2d(72, 9), Math.toRadians(0))
                 .build();
 
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+                //second trajectory moves the robot back to get the second wobble goal
+                //turning 90 degrees while moving back so that the robot lines up with the wobble goal
+                .back(20)
+                .build();
+
+
         waitForStart();
-
-        if(isStopRequested()) return;
-
-        drive.followTrajectory(myTrajectory);
+        drive.followTrajectory(traj1);
+        drive.followTrajectory(traj2);
     }
 
 
